d7d87f50bf13f2546b8df29a59bda4239053ab1f
JOEL PASCHOAL BARINI <1090482211009@fatec-srv.local>
2023-10-06 10:52:56 -0300

    (06/10) Exercícios 01 a 03 de React Hooks

--- react-hooks/src/exercicios/01.jsx
+++ react-hooks/src/exercicios/01.jsx
@@ -2,17 +2,22 @@ import * as React from 'react'
 
 function Greeting({initialName}) {
   // 💣 exclua esta declaração de variável e troque por uma chamada a React.useState
-  const name = ''
+  //const name = ''
+
+  //Dentro dos parenteses do useState() colocamos o VALOR INICIAL da variável de estado
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     // 🐨 atualize o nome aqui baseado em event.target.value
+    setName(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Nome: </label>
-        <input onChange={handleChange} id="name" />
+        {/*Um componente que tem tanto onChange quanto seu value ligados à variável de estado é chamado COMPONENTE CONTROLADO*/}
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Olá {name}</strong> : 'Por favor, informe seu nome'}
     </div>
@@ -20,7 +25,8 @@ function Greeting({initialName}) {
 }
 
 function Exercicio01() {
-  return Greeting({initialName: "Turíbio"})
+  //return Greeting({initialName: "Turíbio"})
+  return <Greeting initialName="Turíbio" />
 }
 
 export default Exercicio01
