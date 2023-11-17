c46f9285b648161d84f947c0f932fec3734f4d23
JOEL PASCHOAL BARINI <1090482211009@fatec-srv.local>
2023-11-17 09:53:24 -0300

    (17/11) Consumo de dados de API remota

--- karangos/src/App.jsx
+++ karangos/src/App.jsx
@@ -4,12 +4,20 @@ import HeaderBar from './ui/HeaderBar'
 import { ThemeProvider } from '@mui/material/styles'
 import theme from './utils/theme'
 import Box from '@mui/material/Box'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { CssBaseline } from '@mui/material'
+
+import Homepage from './pages/Homepage'
+import CarList from './pages/CarList'
+import CustomerList from './pages/CustomerList'
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
       <ThemeProvider theme={theme}>
+        <CssBaseline /> {/* Reseta o CSS padrão e adapta para o tema */}
         <Box sx={{
           width: '100vw', //toda a alrgura da janela
           minHeight: '100vh', //no mínimo, toda a altura da janela
@@ -17,8 +25,16 @@ function App() {
           backgroundColor: 'background.default'
         }}>
           <HeaderBar />
+          <Box sx={{ m: '25px' /*Margem de 5px de todos os lados*/}}>
+            <Routes>
+            <Route path="/" element={ <Homepage /> } />
+            <Route path="/cars" element={ <CarList /> } />
+            <Route path="/customers" element={ <CustomerList /> } />
+          </Routes>
+          </Box>
         </Box>
       </ThemeProvider>
+      </BrowserRouter>
     </>
   )
 }
